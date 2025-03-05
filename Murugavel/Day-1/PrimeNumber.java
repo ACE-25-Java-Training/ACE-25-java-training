a18f10ccac7f60b3625b6acbfb1dccf5e2d2a020
Murugavel-CDW <murugavel.v@cdw.com>
2025-03-05 13:14:29 +0530

    Day-1 PrimeNumber Task Completed

---
+++ Murugavel/Day-1/PrimeNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class PrimeNumber {
+    public static void main(String[] args) {
+        System.out.print("Enter a number: ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int bitRes = num & 1;
+        if(num < 0 || (num > 2 && bitRes == 0)){
+            System.out.println("Not Prime");
+            return;
+        }
+        for(int i = 3;i <= Math.sqrt(num);i += 2){
+            if(num % i == 0){
+                System.out.println("Not Prime");
+                return;
+            }
+        }
+        System.out.println("Prime");
+    }
+}
